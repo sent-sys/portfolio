16e673fb72a7edf4008010450307cc8e6aa7dc09
Matthew Ho <mattewho37@gmail.com>
2021-07-15 16:37:08 -0400

    added some react-reveal animations again

--- src/Home/index.js
+++ src/Home/index.js
@@ -6,19 +6,21 @@ import Nav from "../Layout/Nav";
 export default function Home() {
   return (
     <div className="home-color m-1 p-2 rounded shadow">
-      <Nav />
-      <div>
-        <div className="mt-3 m-5 before-intro"></div>
-        <h1 className="text-white text-center m-3">My name is Matthew Ho</h1>
-        <h1 className="text-white text-center m-3">
-          I build and design ideas on the internet
-        </h1>
-        <div className="mb-3 m-5 after-intro"></div>
-      </div>
       <Fade>
-        <List />
+        <Nav />
+        <div>
+          <div className="mt-3 m-5 before-intro"></div>
+          <h1 className="text-white text-center m-3">My name is Matthew Ho</h1>
+          <h1 className="text-white text-center m-3">
+            I build and design ideas on the internet
+          </h1>
+          <div className="mb-3 m-5 after-intro"></div>
+        </div>
+        <Fade>
+          <List />
+        </Fade>
+        <Footer />
       </Fade>
-      <Footer />
     </div>
   );
 }
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,13 +1,10 @@
 import Home from "../Home";
-import { Fade } from "react-reveal";
 
 export default function Layout() {
   return (
     <div className="main container-fluid">
       <header id="top"> </header>
-      <Fade>
-        <Home />
-      </Fade>
+      <Home />
     </div>
   );
 }
