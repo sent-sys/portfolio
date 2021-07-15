0ee100d0c0b992ed5c9a86eb2b29ffd7a86c6a8c
Matthew Ho <mattewho37@gmail.com>
2021-07-15 13:33:20 -0400

    Added github link to footer; changed copyright

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,11 +1,19 @@
 export default function Footer() {
   return (
-    <div className="footer">
+    <div className="footer align-items-center">
       <div className="row d-flex">
         <div className="col-4 d-flex justify-content-center">
-          <p>1</p>
+          <a
+            className="text-white text-decoration-none link-light git-link"
+            href="https://github.com/sent-sys"
+            target="_blank"
+          >
+            Github
+          </a>
         </div>
-        <div className="col-4 d-flex justify-content-center">2</div>
+        <div className="col-4 d-flex justify-content-center align-items-center">
+          <p className="small text-white">© 2021 Matthew Ho</p>
+        </div>
         <div className="col-4 d-flex justify-content-center">3</div>
       </div>
     </div>
