73e36dde4c10c00e72a513a46cc9f73fcc93fcbf
Matthew Ho <mattewho37@gmail.com>
2021-09-13 17:56:43 -0400

    adding dependencies

--- src/List/index.js
+++ src/List/index.js
@@ -22,68 +22,24 @@ export default function List() {
                   Reservation system; built with React, Node.js, Express, and
                   Bootstrap.
                 </p>
-                <Link
-                  to="https://github.com/sent-sys/portfolio"
-                  className="btn btn-primary"
-                >
-                  Github
-                </Link>
-                <Link
-                  to="https://fc-restaurant-res-frontend.herokuapp.com/"
-                  className="btn btn-warning"
-                >
-                  Heroku Live Demo
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 col-sm-6 col-md-4">
-            <div className="card text-white card-bg shadow my-2">
-              <img
-                src="images/2.jpg"
-                className="card-img-top"
-                alt="Screenshot placeholder"
-              />
-              <div className="card-body">
-                <h5 className="card-title text-center">Project 2</h5>
-                <p className="card-text">Quick project description.</p>
-                <Link to="/" className="btn btn-primary">
-                  (placeholder GITHUB link)
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 col-sm-6 col-md-4">
-            <div className="card text-white card-bg shadow my-2">
-              <img
-                src="images/3.jpg"
-                className="card-img-top"
-                alt="Screenshot placeholder"
-              />
-              <div className="card-body">
-                <h5 className="card-title text-center">Project 3</h5>
-                <p className="card-text">Quick project description.</p>
-                <Link to="/" className="btn btn-primary">
-                  (placeholder GITHUB link)
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row p-2">
-          <div className="col-12 col-sm-6 col-md-4">
-            <div className="card text-white card-bg shadow my-2">
-              <img
-                src="images/1.jpg"
-                className="card-img-top"
-                alt="Screenshot placeholder"
-              />
-              <div className="card-body">
-                <h5 className="card-title text-center">Project 1</h5>
-                <p className="card-text">Quick project description.</p>
-                <Link to="/" className="btn btn-primary">
-                  (placeholder GITHUB link)
-                </Link>
+                <div className="d-flex justify-content-around">
+                  <a
+                    href="https://github.com/sent-sys/portfolio"
+                    className="btn btn-primary"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Github
+                  </a>
+                  <a
+                    href="https://fc-restaurant-res-frontend.herokuapp.com/"
+                    className="btn btn-warning"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Heroku Live Demo
+                  </a>
+                </div>
               </div>
             </div>
           </div>
